4adae2afd19283c77840e0c4c1c73bab3943aa05
kerniusmatulis <Kernius.matulis@gmail.com>
2018-04-26 15:26:54 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-
+import { LandingComponent } from './landing/landing.component';
+import { NavbarComponent } from './ui/navbar/navbar.component';
+import { FooterComponent } from './ui/footer/footer.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProjectFormComponent } from './projects/project-form/project-form.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProjectDetailsComponent } from './projects/project-details/project-details.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProjectsComponent,
+    ProjectFormComponent,
+    ProfileComponent,
+    ProjectDetailsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
